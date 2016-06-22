f8a7dc18ccfc85adb7ec3f49eb2aa5606323dd42
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2016-06-22 12:01:29 +0200

    op nfs: test of heel het path naar werk-directory voor iedereen executable is

--- alpino.bash
+++ alpino.bash
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+script='
+@parts = ("/");
+$p = "";
+foreach $part (split m!/!, $ENV{dir}) {
+    if ($part ne "") {
+	$p .= "/" . $part;
+	push @parts, $p;
+    }
+}
+foreach $p (@parts) {
+    $s = `stat -c %A "$p"`;
+    if ($s =~ /d(...){0,2}..-/) {
+        print "$p";
+	exit;
+    }
+}
+'
+
 if [ $# != 1 ]
 then
     echo Usage: $0 workdir
@@ -8,17 +26,38 @@ fi
 
 if [ ! -d "$1" -o ! -r "$1" ]
 then
+    echo
     echo "'$1'" is not a readable directory
+    echo
     exit
 fi
 
-case $1 in
+case "$1" in
     /*)
 	;;
     *)
+	echo
 	echo workdir must be an absolute path
 	echo "'$1'" is not an absolute path
+	echo
 	exit
+	;;
+esac
+
+st=`stat -f -c %T "$1"`
+case "$st" in
+    nfs*)
+	P=`dir="$1" perl -e "$script"`
+	if [ "$P" != "" ]
+	then
+	    echo
+	    echo path "'$P'" must be executable for everybody
+	    echo please do:
+	    echo
+	    echo "  chmod a+x $P"
+	    echo
+	    exit
+	fi
 	;;
 esac
 
