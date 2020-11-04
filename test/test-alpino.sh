f86a888ad99d6501ce4faaefc7eb5b94eddabe2a
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2020-11-04 12:29:41 +0100

    update voor test/test-alpino.sh

--- test/test-alpino.sh
+++ test/test-alpino.sh
@@ -3,9 +3,9 @@
 rm out/*.xml
 ../alpino.bash `pwd` /bin/sh -c 'partok /work/voorbeelden/weerbericht.txt | Alpino -flag treebank /work/data/out debug=1 end_hook=xml user_max=900000 -parse' | tee out/log
 echo
-echo THERE SHOULD BE NO DIFFERENCES:
+echo 'THERE SHOULD BE NO DIFFERENCES EXCEPT FOR <parser ... date /> :'
 echo
 for i in ori/*.xml
 do
-    diff -s $i out/`basename $i`
+    diff -U0 -s $i out/`basename $i`
 done
